eefa57234e69e92c73294e9d94e33e9147fd1aa3
Kyle Stark <kyle@goodrx.com>
2016-09-08 15:41:14 -0700

    Add minimal setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SudsSigner",
+    version="1.0.0",
+    classifiers=[
+        'Private :: Do Not Upload',
+    ],
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
