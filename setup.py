10eee25103e6748d835a65da483c726ec7b38882
Kyle <kyle@goodrx.com>
2016-09-08 15:49:02 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SudsSigner",
-    version="1.0.0",
+    version="2.0.0",
     classifiers=[
         'Private :: Do Not Upload',
     ],
